24733750e9fc760212f1a8c16e103faf8e13bbfe
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-02-11 11:12:54 -0800

    Update text so I can see it all working

--- Main.hs
+++ Main.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Web.Scotty
+import           Web.Scotty
 
 main :: IO ()
 main = scotty 3000 $ do
   get "/:word" $ do
     beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+    html $ mconcat ["<h1>Erin says: ", beam, "</h1>"]
