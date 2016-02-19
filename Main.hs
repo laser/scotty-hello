48d9ee8e7325f12b91a003aa17cc9b0d2730797c
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-02-19 08:46:36 -0800

    Connect to PostgreSQL and respond with random number

--- Main.hs
+++ Main.hs
@@ -1,9 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import           Control.Monad                        (liftM)
+import           Control.Monad.IO.Class               (liftIO)
+import           Data.Text.Lazy                       (pack)
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+import           System.Environment                   (getEnv)
 import           Web.Scotty
 
+import qualified Database.PostgreSQL.Simple           as PostgreSQL
+
 main :: IO ()
-main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Erin screams: ", beam, "</h1>"]
+main = do
+  cfg <- PostgreSQL.ConnectInfo <$> getEnv "DATABASE_HOST"
+                                <*> liftM read (getEnv "DATABASE_PORT")
+                                <*> getEnv "DATABASE_USER"
+                                <*> getEnv "DATABASE_PASSWORD"
+                                <*> getEnv "DATABASE_NAME"
+  conn <- PostgreSQL.connect cfg
+  scotty 8080 $ do
+    middleware logStdoutDev
+
+    get "/:word" $ do
+      [PostgreSQL.Only i] <- liftIO $ PostgreSQL.query_ conn "select floor(random()*(10-1)+1)"
+      beam <- param "word"
+      html $ mconcat ["<h1>Erin screams: ", (pack . show $ (i :: Double)), beam, "</h1>"]
